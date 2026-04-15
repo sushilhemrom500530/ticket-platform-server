d484441cc68492944f431ca308ab49d73a4f5a81
sushilhemrom500530 <sushilhemrom98@gmail.com>
2026-04-15 22:04:47 +0600

    add project initiate and connect to db

---
+++ src/app.ts
@@ -0,0 +1,20 @@
+import express, { type Request, type Response } from "express";
+import cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan";
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.use(helmet());
+app.use(morgan("dev"));
+
+// app.use("/api/v1", routes);
+app.use("/", (req: Request, res: Response) => {
+  res.send({
+    message: "server is running....",
+  });
+});
+
+export default app;
